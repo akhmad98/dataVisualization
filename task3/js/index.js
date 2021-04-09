6b43beee08d46b1434468f5b8bad3f8a54bd4e2d
akhmadbadalov98@gmail.com <akhmadbadalov98@gmail.com>
2021-04-09 14:40:36 +0500

    textaddedtograph

--- task3/js/index.js
+++ task3/js/index.js
@@ -32,70 +32,76 @@ var xScale = d3.scaleBand()
 .range([0, svgWidth]).padding([0.1]);
 
 d3.csv('https://gist.githubusercontent.com/akhmad98/52cc680868e6859e50a12bb7ee8942fb/raw/005b11f85bef534ea4cabbc01a06d1cad0c3fd49/data.csv')
-.then(function(data) { 
-// var len=data.map(function(d) {return d.letter});
+    .then(function (data) {
+        // var len=data.map(function(d) {return d.letter});
 
-//console.log();
-xScale.domain(data.map(function(d) {return d.letter;}))
-//xScale.domain(len);
-yScale.domain([0, d3.max(data, function(d) {return d.frequency;})]);
+        //console.log();
+        xScale.domain(data.map(function (d) { return d.letter; }))
+        //xScale.domain(len);
+        yScale.domain([0, d3.max(data, function (d) { return d.frequency; })]);
 
-var grpXAxs = chart.append("g")
-      .attr("transform", `translate(0, ${svgHeight})`);
-var grpYAxs = chart.append("g");
+        var grpXAxs = chart.append("g")
+            .attr("transform", `translate(0, ${svgHeight})`);
+        var grpYAxs = chart.append("g");
 
-var x_axis = d3.axisBottom(xScale); 
-var y_axis = d3.axisLeft(yScale).tickFormat( function (d) {
-    return "".concat((d * 100).toFixed(0), "%");
-});
- grpXAxs.call(x_axis);
- grpYAxs.call(y_axis);
+        var x_axis = d3.axisBottom(xScale);
+        var y_axis = d3.axisLeft(yScale).tickFormat(function (d) {
+            return "".concat((d * 100).toFixed(0), "%");
+        });
+        grpXAxs.call(x_axis);
+        grpYAxs.call(y_axis);
 
-chart.append("g")
-      .attr("class", "grid")
-      .attr("fill", "#d3d3d3")
-      .attr("transform", `translate (0, ${svgHeight})`)
-      .style("color", "#d3d3d3")
-      .call(x_gride(xScale).tickSize(-svgHeight).tickFormat(""));
-chart.append("g")
-      .attr("class", "yAxis")
-      .attr("fill", "none")
-      .style("color", "#d3d3d3")
-      .call(y_gride(yScale).tickSize(-svgWidth).tickFormat(""))
+        chart.append("g")
+            .attr("class", "grid")
+            .attr("fill", "#d3d3d3")
+            .attr("transform", `translate (0, ${svgHeight})`)
+            .style("color", "#d3d3d3")
+            .call(x_gride(xScale).tickSize(-svgHeight).tickFormat(""));
+        chart.append("g")
+            .attr("class", "yAxis")
+            .attr("fill", "none")
+            .style("color", "#d3d3d3")
+            .call(y_gride(yScale).tickSize(-svgWidth).tickFormat(""))
  
-      var tooltip = d3.select("body").append("div").attr("id", "tool-tip");
+        var tooltip = d3.select("body").append("div").attr("id", "tool-tip");
 
-      chart.append("path")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "#387B21")
-      .attr("stroke-width", 2)
-      .attr("d", d3.line()
-        .x(function(d) { return xScale(d.letter) })
-        .y(function(d) { return yScale(d.frequency) })
-        .curve(d3.curveMonotoneX)
-        )
-    chart
-        .append("g")
-        .selectAll("dot")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("cx", function (d) { return xScale(d.letter)})
-        .attr("cy", function (d) { return yScale(d.frequency)})
-        .attr("r", 5)
-        .style("fill", "#387B21")
-        .style("fcursor", "pointer")
-        .on("mouseover", function (b, e) {
-          tooltip.style("visibility", "visible")
-              // .style("left", "")//e.path.getBoundingClientRect().x - tooltip.node().clientWidth / 2 + b.path[0].getBoundingClientRect().width / 2 + "px")
-              // .style("top", "100px")//d.path[0].getBoundingClientRect().y - 60 + "px")
-              .html('<span class="tool-tip-title">Frequnecy: </span><span class="tool-tip-value">'.concat(b.frequency, "</span>"));  
-              })
-              .on("mousemove", function () {
-              return tooltip.style("top", (d3.event.pageY -30) + "px").style("left", (d3.event.pageX + 10) + "px");
-              })
-              .on("mouseout", function (d) {
-              return tooltip.style("visibility", "hidden");
-              });
+        chart.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", "#387B21")
+            .attr("stroke-width", 2)
+            .attr("d", d3.line()
+                .x(function (d) { return xScale(d.letter) })
+                .y(function (d) { return yScale(d.frequency) })
+                .curve(d3.curveMonotoneX)
+            )
+        chart
+            .append("g")
+            .selectAll("dot")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) { return xScale(d.letter) })
+            .attr("cy", function (d) { return yScale(d.frequency) })
+            .attr("r", 5)
+            .style("fill", "#387B21")
+            .style("fcursor", "pointer")
+            .on("mouseover", function (b, e) {
+                tooltip.style("visibility", "visible")
+                    // .style("left", "")//e.path.getBoundingClientRect().x - tooltip.node().clientWidth / 2 + b.path[0].getBoundingClientRect().width / 2 + "px")
+                    // .style("top", "100px")//d.path[0].getBoundingClientRect().y - 60 + "px")
+                    .html('<span class="tool-tip-title">Frequnecy: </span><span class="tool-tip-value">'.concat(b.frequency, "</span>"));
+            })
+            .on("mousemove", function () {
+                return tooltip.style("top", (d3.event.pageY - 30) + "px").style("left", (d3.event.pageX + 10) + "px");
+            })
+            .on("mouseout", function (d) {
+                return tooltip.style("visibility", "hidden");
+            });
+        chart.append("text")
+            .attr("x", (svgWidth / 2))
+            .attr("y", 0 + (mrg.top / 2))
+            .attr("text-anchor", "middle")
+            .text("Relative Frequency of Letters in the English Alphabet");
+
 });
